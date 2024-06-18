105d08e76d53dc0cbd761a5e52d1e6657b142ca7
Abu Tyeb Azad <mausulazad495@gmail.com>
2024-06-18 02:10:17 +0000

    Code for web-serive (Flask) based prediction model (using docker) is added

---
+++ 04-deployment/web-service/test.py
@@ -0,0 +1,26 @@
+'''
+import predict
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40  
+}
+
+
+features = predict.prepare_features(ride)
+pred = predict.predict(features)
+print(pred)
+'''
+
+import requests
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40  
+}
+
+url = 'http://127.0.0.1:9696/predict'
+response = requests.post(url, json=ride)
+print(response.json())
